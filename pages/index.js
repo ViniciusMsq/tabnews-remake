b37d64ff978f9233db2ce323c28d8b2cd19f0494
Vinicius Mesquini <viniciusm.gc@outlook.com>
2023-10-16 21:05:21 -0300

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){ 
-    return <h1>Studway - O melhor jeito de aprender é ensinar! Love U pituchinha </h1>
+function Home() {
+  return (
+    <h1>Studway - O melhor jeito de aprender é ensinar! Love U pituchinha </h1>
+  );
 }
 
 export default Home;
