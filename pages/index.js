0fa49fa3f9d7aae865904ff3590b69d911c5342b
Vinicius Mesquini <viniciusm.gc@outlook.com>
2025-01-19 13:37:32 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Ecossistema Studway em desenvolvimento!</h1>
+function Home() {
+  return <h1>Ecossistema Studway em desenvolvimento!</h1>;
 }
 
 export default Home;
