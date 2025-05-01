8fd31ece1b0728ab1b49da6f795a1907bd3fe5e4
Vinicius Mesquini <viniciusm.gc@outlook.com>
2025-05-01 15:11:14 +0000

    add `/api/v1/migrations`

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,11 +1,32 @@
+import database from "infra/database.js";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
+  expect(response1.status).toBe(201);
 
-  const respondeBody = await response.json();
-  console.log(respondeBody);
-
+  const respondeBody = await response1.json();
   expect(Array.isArray(respondeBody)).toBe(true);
+  expect(respondeBody.length).toBeGreaterThan(0);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response2.status).toBe(200);
+
+  const respondeBody2 = await response2.json();
+  expect(Array.isArray(respondeBody2)).toBe(true);
+  expect(respondeBody2.length).toBe(0);
+
+  const data = await database.query(
+    "SELECT COUNT(*)::int inserido FROM public.pgmigrations;",
+  );
+  expect(data.rows[0].inserido).toBeGreaterThan(0);
 });
